a9bbc6674b7cea1e3c6de844e05346b88e92179c
CHENj <1790993370@qq.com>
2024-07-07 21:54:44 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('mobile-menu').addEventListener('click', function() {
+    var navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('active');
+});
